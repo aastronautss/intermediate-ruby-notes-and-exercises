a6fa4174061695716b0962c67463690b00db6654
Tyler Guillen <tyguillen@gmail.com>
2016-02-11 15:19:18 -0500

    initial commit

---
+++ lesson-1/my_each.rb
@@ -0,0 +1,12 @@
+def my_each(array)
+  iterator = 0
+
+  while iterator < array.length
+    yield(array[iterator])
+    iterator += 1
+  end
+
+  arry
+end
+
+each([1, 2, 3, 4, 5]) { |num| puts num }
